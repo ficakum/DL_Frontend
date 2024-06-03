ed94c49626271b2d7723a710bc3201487b9a2854
ficanikolic <filip.nikolic@nistruct.com>
2024-06-03 09:06:53 +0200

    Add app initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+import { ROUTES } from 'constants/routes'
+import Auth from 'pages/Auth'
+
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path={ROUTES.AUTH.PATH} element={<Auth />}></Route>
+
+          {/* wild route*/}
+          <Route path='*' element={<Navigate to={ROUTES.AUTH.PATH} replace />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
